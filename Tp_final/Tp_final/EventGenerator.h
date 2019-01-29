75e0d406c420f24704f2717491b96c2d7efaea54
taomasgonzalez <togonzalez@itba.edu.ar>
2019-01-28 22:26:36 -0300

    cambios en evetGen

--- Tp_final/Tp_final/EventGenerator.h
+++ Tp_final/Tp_final/EventGenerator.h
@@ -7,8 +7,6 @@
 #include <queue> 
 
 
-
-
 class EventGenerator
 {
 public:
@@ -24,6 +22,7 @@ public:
 	void append_new_net_event(EventPackage* new_ev_pack);
 	void append_new_soft_event(EventPackage* new_ev_pack);
 	void append_new_allegro_event(EventPackage* new_ev_pack);
+
 	EventPackage * fetch_event_al();
 	EventPackage * fetch_event_net();
 	EventPackage * fetch_event_soft();
@@ -35,7 +34,6 @@ protected:
 	std::queue<EventPackage*>* soft_queue;		//cola para eventos de software.
 	std::queue<EventPackage*>* net_queue;		//cola para eventos de networking.
 	std::queue<EventPackage*>* allegro_queue;		//cola para eventos de networking.
-
 
 
 	ALLEGRO_TIMER * time_out_timer;  // has to be moved to allegro.cpp??
