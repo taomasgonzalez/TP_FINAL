0d12a87c701c48f7343edd71e3ac68c205e94119
gpanaggio <31071203+gpanaggio@users.noreply.github.com>
2019-02-06 20:43:55 -0300

    Interfaz gráfica

--- Tp_final/Tp_final/GraficInterface.h
+++ Tp_final/Tp_final/GraficInterface.h
@@ -1,15 +1,6 @@
 #pragma once
 
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_color.h> 
-#include <allegro5/allegro_primitives.h> 
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_audio.h> 
-#include <allegro5/allegro_acodec.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-
-//#include "AllegroClass.h"
+#include "AllegroClass.h"
 
 #include <iostream>
 #include <string>
