9ebf3e056549f1872eedca8f3bb1680a54fd0c8b
taomasgonzalez <togonzalez@itba.edu.ar>
2019-02-07 00:18:00 -0300

    comence a comentar todo el codigo

--- Tp_final/Tp_final/Map.cpp
+++ Tp_final/Tp_final/Map.cpp
@@ -31,57 +31,184 @@ Map::~Map()
 	delete dijkstra_manager;
 }
 
+
+/******************************************
+**************get_all_enemies**************
+*******************************************
+*get_all_enemies returns a pointer to an Enemy pointer 
+*vector that contains all enemies on the map, both dead and alive.
+*	INPUT:
+		1) void.	
+*	OUTPUT:
+*		pointer to an Enemy pointer vector with the characteristics given by the description. 
+*/
 std::vector<Enemy*>* Map::get_all_enemies()
 {
 	return all_enemies;
 }
-
+/******************************************
+*****************get_all_proyectiles*******
+*******************************************
+*get_all_proyectiles returns a pointer to a Proyectile pointer 
+*vector that contains all proyectiles on the map.
+*	INPUT:
+		1)	void.
+*	OUTPUT:
+*		pointer to a Proyectile pointer vector with the characteristics given by the description.
+*/
 std::vector<Proyectile*>* Map::get_all_proyectiles()
 {
 	return all_proyectiles;
 }
-
+/******************************************
+*****************get_all_players***********
+*******************************************
+*get_all_players returns a pointer to a Player pointer vector 
+*that contains all players on the map, both dead and alive.
+*	INPUT:
+1)
+*	OUTPUT:
+*		pointer to a Player pointer vector with the characteristics given by the description.
+*/
 std::vector<Player*>* Map::get_all_players()
 {
 	return all_players;
 }
-
+/******************************************
+**********give_me_the_original_map*********
+*******************************************
+*give_me_the_original_map returns the map's original distribution in a char array.
+*This distribution has the same format as the char array received and sent by networking.
+*Each char represents the same MapThing as the communication protocol establishes.
+*	INPUT:
+		1)	void.
+*	OUTPUT:
+*		char pointer that contains a string (without the terminator) with the chars that 
+*		give the map the original level distribution. 
+*/
 const char * Map::give_me_the_original_map() {
 
 	return this->original_distribution;
 }
 
+/******************************************
+**************give_me_the_checksum*********
+*******************************************
+*give_me_the_checksum
 
+*	INPUT:
+*		1)	void.
+*	OUTPUT:
+*
+*/
 unsigned char  Map::give_me_the_checksum() {
 
 	return this->my_checksum;
 }
-
+/******************************************
+**************cell_has_proyectiles*********
+*******************************************
+*cell_has_proyectiles informs the user whether a given map cell
+*contains one or more proyectiles of any kind.
+*	INPUT:
+*		1)coord_x :	row of the cell to be checked.
+*		2)coord_y : column of the cell to be checked
+*
+*	OUTPUT:
+*		a boolean that is true if the given cell contains a proyectile. False otherwise. 
+*/
 bool Map::cell_has_proyectiles(int coord_x, int coord_y) {
 	return get_cell(coord_x, coord_y).has_proyectiles();
 }
+/******************************************
+**************cell_has_players*************
+*******************************************
+*cell_has_players informs the user whether a given map cell
+*contains one or more players.
+*	INPUT:
+*		1)coord_x :	row of the cell to be checked.
+*		2)coord_y : column of the cell to be checked
+*
+*	OUTPUT:
+*		a boolean that is true if the given cell contains a player. False otherwise.
+*/
 bool Map::cell_has_players(int coord_x, int coord_y) {
 	return get_cell(coord_x, coord_y).has_players();
 }
+/******************************************
+**************cell_has_enemies*************
+*******************************************
+*cell_has_enemiesinforms the user whether a given map cell
+*contains one or more enemies.
+*	INPUT:
+*		1)coord_x :	row of the cell to be checked.
+*		2)coord_y : column of the cell to be checked
+*
+*	OUTPUT:
+*		a boolean that is true if the given cell contains an enemy. False otherwise.
+*/
 bool Map::cell_has_enemies(int coord_x, int coord_y) {
 	return get_cell(coord_x, coord_y).has_enemies();
 }
-
+/******************************************
+**************get_cell*********************
+*******************************************
+*get_cell returns a MapCell object that contains 
+*all the MapThings for a given map cell.
+*	INPUT:
+*		1)coord_x :	row of the cell of which the MapCell is needed.
+*		2)coord_y : column of the cell of which the MapCell is needed. 
+*
+*	OUTPUT:
+*		a MapCell with the characteristics mentioned in the method description.
+*/
 MapCell Map::get_cell(int coord_x, int coord_y) {
 	return map_cells[coord_x][coord_y];
 }
-
+/******************************************
+**************clear************************
+*******************************************
+*clear removes all MapThings from the map. 
+*Note that it only removes MapThings, so that all MapCells on the map
+*will remain untouched an hence should not be created again to place anything inside the map again.
+*	INPUT:
+*		1) void.
+*
+*	OUTPUT:
+*		void.
+*/
 void Map::clear()
 {
 	for (int i = 0; i < number_of_rows; i++)
 		for (int j = 0; j < number_of_columns; j++)
 			get_cell(i, j).clear();
 }
-
+/******************************************
+***************get_cell_enemies************
+*******************************************
+*get_cell_enemies returns a vector containing all enemies contained on a given map cell.
+*Note that the given map cell will still contain the enemies after this method is called.
+*	INPUT:
+*		1) coord_x : row of the cell from which the user wants to retrieve all enemies.
+*		2) coord_y : column of the cell from which the user wants to retrieve all enemies.
+*	OUTPUT:
+*		a vector of Enemy pointers containing all enemies for the given map cell.
+*/
 std::vector<Enemy*> Map::get_cell_enemies(int coord_x, int coord_y) {
 	return get_cell(coord_x, coord_y).get_enemies();
 }
-
+/******************************************
+*********cell_has_enemy_proyectiles********
+*******************************************
+*cell_has_enemy_proyectiles informs the user whether a given cell contains 
+*one or more proyectiles that could kill a player 
+*(a proyectile created by an enemy, hence an enemy proyectile) 
+*	INPUT:
+*		1)coord_x : row of the cell to be checked.
+*		2)coord_y : column of the cell to be checked.
+*	OUTPUT:
+*		a boolean that is true if the cell contains any enemy proyectiles. False otherwise
+*/
 bool Map::cell_has_enemy_proyectiles(int coord_x, int coord_y)
 {
 	if (cell_has_proyectiles(coord_x, coord_y)) {
@@ -95,7 +222,18 @@ bool Map::cell_has_enemy_proyectiles(int coord_x, int coord_y)
 	return false;
 
 }
-
+/******************************************
+*********cell_has_player_proyectiles********
+*******************************************
+*cell_has_player_proyectiles informs the user whether a given cell contains
+*one or more proyectiles that could kill an enemy
+*(a proyectile created by a player, hence a player proyectile)
+*	INPUT:
+*		1)coord_x : row of the cell to be checked.
+*		2)coord_y : column of the cell to be checked.
+*	OUTPUT:
+*		a boolean that is true if the cell contains any player proyectiles. False otherwise
+*/
 bool Map::cell_has_player_proyectiles(int coord_x, int coord_y)
 {
 	if (cell_has_proyectiles(coord_x, coord_y)) {
@@ -108,20 +246,62 @@ bool Map::cell_has_player_proyectiles(int coord_x, int coord_y)
 	return false;
 
 }
-
+/******************************************
+*************cell_has_floor****************
+*******************************************
+*cell_has_floor informs the user whether a given cell contains floor that prevents 
+*movement to that cell or not.
+*	INPUT:
+*		1)coord_x : row of the cell to be checked.
+*		2)coord_y : column of the cell to be checked.
+*	OUTPUT:
+*		a boolean that is true if the cell contains a 'floor' MapThing. False otherwise.
+*/
 bool Map::cell_has_floor(int coord_x, int coord_y)
 {
 	return get_cell(coord_x, coord_y).has_floor();
 }
-
+/******************************************
+**************get_cell_players*************
+*******************************************
+* get_cell_players returns a vector of player pointers 
+* that contains all players that are currently inside a given cell.
+* the user should call the method cell_has_players before calling this function to make
+* sure that the cell has players.
+*	INPUT:
+*		1)coord_x : row of the cell from which the players will be retrieved.
+*		2)coord_y : column of the cell from which the players will be retrieved.
+*	OUTPUT:
+*		a vector containing player pointers to all the players that are currently on the given map cell.
+*/
 std::vector<Player*> Map::get_cell_players(int coord_x, int coord_y) {
 	return get_cell(coord_x, coord_y).get_players();
 }
-
+/******************************************
+*************get_cell_proyectiles**********
+*******************************************
+*get_cell_proyectiles returns a vector of proyectiles pointers 
+* that contains all proyectiles that are currently inside a given cell.
+* the user should call the method cell_has_proyectiles before calling this function to make
+* sure that the cell has proyectiles.
+*	INPUT:
+*		1) coord_x : row of the cell from which the players will be retrieved.
+*		2) coord_y : column of the cell from which the players will be retrieved.
+*	OUTPUT:
+*		a vector containing proyectiles pointers to all the proyectiles that are currently on the given map cell.
+*/
 std::vector<Proyectile*> Map::get_cell_proyectiles(int coord_x, int coord_y) {
 	return get_cell(coord_x, coord_y).get_proyectiles();
 }
-
+/******************************************
+**************get_from_map*****************
+*******************************************
+*get_from_map retrieves a MapThing pointer from the map.
+*	INPUT:
+		1) id : id of the MapThing to be retrieved from the map.
+*	OUTPUT:
+*		MapThing pointer retrieved from the map.
+*/
 MapThing * Map::get_from_map(unsigned int id) {
 	MapThing* gotten = NULL;
 	for (int i = 0; i < number_of_rows; i++)
@@ -131,12 +311,33 @@ MapThing * Map::get_from_map(unsigned int id) {
 
 	return gotten;
 }
-
+/******************************************
+**************get_from_map*****************
+*******************************************
+*get_from_map is an internal function for map. 
+*given the cell coordinates and the z coordinate (the position of the MapThing inside the MapCell array)
+*it retrieves the MapThing from the map.
+*	INPUT:
+		1)coord_x :  
+		2)coord_y : 
+		3)coord_z :
+*	OUTPUT:
+*
+*/
 MapThing * Map::get_from_map(int coord_x, int coord_y, int coord_z)
 {
 	return get_cell(coord_x, coord_y).get_floor(coord_z);
 }
+/******************************************
+****************************
+******************************************
+*
 
+*	INPUT:
+1)
+*	OUTPUT:
+*
+*/
 bool Map::delete_from_map(unsigned int id) {
 	bool successfully_deleted = false;
 
@@ -150,7 +351,16 @@ bool Map::delete_from_map(unsigned int id) {
 		}
 	return successfully_deleted;
 }
+/******************************************
+****************************
+******************************************
+*
 
+*	INPUT:
+1)
+*	OUTPUT:
+*
+*/
 bool Map::delete_from_map(MapThing * thing) {
 
 	bool successfully_deleted = false;
@@ -163,29 +373,86 @@ bool Map::delete_from_map(MapThing * thing) {
 
 	return successfully_deleted;
 }
+/******************************************
+****************************
+******************************************
+*
+
+*	INPUT:
+1)
+*	OUTPUT:
+*
+*/
 void Map::print_cell(int coord_x, int coord_y)
 {
 	get_cell(coord_x, coord_y).print();
 }
+/******************************************
+****************************
+******************************************
+*
+
+*	INPUT:
+1)
+*	OUTPUT:
+*
+*/
 const char * Map::get_last_loaded_distribution()
 {
 	return original_distribution;
 }
+/******************************************
+****************************
+******************************************
+*
 
+*	INPUT:
+1)
+*	OUTPUT:
+*
+*/
 Position Map::find_next_movement_4_shortest_path(int from_x, int from_y, int to_x, int to_y)
 {
 	return dijkstra_manager->get_next_movement_shortest_path(from_x, from_y, to_x, to_y);
 }
+/******************************************
+****************************
+******************************************
+*
 
+*	INPUT:
+1)
+*	OUTPUT:
+*
+*/
  int Map::get_number_of_rows()
 {
 	return number_of_rows;
 }
+ /******************************************
+ ****************************
+ ******************************************
+ *
+
+ *	INPUT:
+ 1)
+ *	OUTPUT:
+ *
+ */
  int Map::get_number_of_columns()
  {
 	 return number_of_columns;
  }
+ /******************************************
+ ****************************
+ ******************************************
+ *
 
+ *	INPUT:
+ 1)
+ *	OUTPUT:
+ *
+ */
 bool Map::move_id(unsigned int id, int final_x, int final_y) {
 	bool moved = false;
 	MapThing * thingy = NULL;
@@ -196,12 +463,30 @@ bool Map::move_id(unsigned int id, int final_x, int final_y) {
 		}
 	return moved;
 }
+/******************************************
+****************************
+******************************************
+*
 
+*	INPUT:
+1)
+*	OUTPUT:
+*
+*/
 bool Map::move_map_thing(MapThing * thing, int final_x, int final_y)
 {
 	return move_id(thing->id, final_x, final_y);
 }
+/******************************************
+****************************
+******************************************
+*
 
+*	INPUT:
+1)
+*	OUTPUT:
+*
+*/
 void Map::place_on_map(int coord_x, int coord_y, MapThing* thing) {
 	MapCell cell = get_cell(coord_x, coord_y);
 	cell.place_on_cell(thing);
@@ -210,7 +495,16 @@ void Map::place_on_map(int coord_x, int coord_y, MapThing* thing) {
 	place_on_map_thing_vectors(thing);
 	
 }
+/******************************************
+****************************
+******************************************
+*
 
+*	INPUT:
+1)
+*	OUTPUT:
+*
+*/
 void Map::place_on_map(int coord_x, int coord_y, Item_type identifyer, Sense_type direction)
 {
 	place_on_map(coord_x, coord_y, map_filler.create_map_thing(identifyer, direction));
