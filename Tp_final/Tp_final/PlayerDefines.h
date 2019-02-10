db88d245eb8cc0ffc086823540cdfff88f1b55fd
gpanaggio <31071203+gpanaggio@users.noreply.github.com>
2019-02-10 01:13:56 -0300

    Add files via upload

--- Tp_final/Tp_final/PlayerDefines.h
+++ Tp_final/Tp_final/PlayerDefines.h
@@ -1,20 +1,20 @@
 #pragma once
 
-#define WALKING_PICS	(12)				// cantidad de imagenes del personaje caminando
-#define JUMPING_PICS	(10)
-#define ATTACKING_PICS	(3)
-#define IDLE_PICS		(1)
+#define WALKING_PICS	(10)				// cantidad de imagenes del personaje caminando
+#define JUMPING_PICS	(1)
+#define ATTACKING_PICS	(10)
+#define IDLE_PICS		(10)
 #define FALLING_PICS	(1)
-#define PUSHING_PICS	(5)
-#define DYING_PICS		(5)
+#define PUSHING_PICS	(10)
+#define DYING_PICS		(10)
 
 #define MOVE_RATIO	(0.3)							// segundos que tarda el personaje en recorrer un bloque
 #define VELOCITY_X (BLOCK_SIZE/(FPS*MOVE_RATIO))	// pixeles que va a recorrer el personaje por cada frame de refresco
-#define MOVE_RATIO_PUSHED	(0.5)
+#define MOVE_RATIO_PUSHED	(1.0)
 #define VEL_PUSHED	(BLOCK_SIZE/(FPS*MOVE_RATIO_PUSHED))
 
 #define FOLDER_CHARA	("characters")				// carpeta donde estan contenidas todas las imagenes de los personajes
-#define FOLDER_TOM		("TOM")						// carpeta para cada personaje en particular
+#define FOLDER_TOM		("TOM (2)")						// carpeta para cada personaje en particular
 #define FOLDER_NICK		("NICK")
 
 #define FOLDER_WALKING	("WALKING")					// carpetas que vas a contener imagenes de una categoria
@@ -25,18 +25,18 @@
 #define FOLDER_PUSHING		("PUSHING")
 #define FOLDER_DYING		("DYING")
 
-#define FILE_TOM_WALKING	("Super Mario Running-F")			// nombre generico de los archivos .png de tom caminando
-#define FILE_TOM_JUMPING	("homerdance-F")
-#define FILE_TOM_IDLE		("tomIdle-F")
-#define FILE_TOM_ATTACKING	("Explosion 2-F")
-#define FILE_TOM_FALLING	("Sonic Running-F")
-#define FILE_TOM_PUSHING	("")
-#define FILE_TOM_DYING		("")
+#define FILE_TOM_WALKING	("JK_P_Gun__Run_00")			// nombre generico de los archivos .png de tom caminando
+#define FILE_TOM_JUMPING	("JK_P_Gun__Jump_00")
+#define FILE_TOM_IDLE		("JK_P_Gun__Idle_00")
+#define FILE_TOM_ATTACKING	("JK_P_Gun__Attack_00")
+#define FILE_TOM_FALLING	("JK_P_Gun__Jump_00")
+#define FILE_TOM_PUSHING	("JK_P_Gun__Hurt_00")
+#define FILE_TOM_DYING		("JK_P__Die_00")
 
-#define FILE_NICK_WALKING	("nickWalk-F")			// nombre generico de los archivos png de tom caminando
-#define FILE_NICK_JUMPING	("nickJump-F")
-#define FILE_NICK_IDLE		("nickIdle-F")
-#define FILE_NICK_ATTACKING	("nickAttack-F")
-#define FILE_NICK_FALLING	("nickFall-F")
-#define FILE_NICK_PUSHING	("nickPush-F")
-#define FILE_NICK_DYING		("nickDie-F")
+#define FILE_NICK_WALKING	("E_E_Gun__Run_000_00")			// nombre generico de los archivos png de tom caminando
+#define FILE_NICK_JUMPING	("E_E_Gun__Jump_00")
+#define FILE_NICK_IDLE		("E_E_Gun__Idle_00")
+#define FILE_NICK_ATTACKING	("E_E_Gun__Attack_00")
+#define FILE_NICK_FALLING	("E_E_Gun__Jump_00")
+#define FILE_NICK_PUSHING	("E_E_Gun__Hurt_00")
+#define FILE_NICK_DYING		("E_E__Die_00")
