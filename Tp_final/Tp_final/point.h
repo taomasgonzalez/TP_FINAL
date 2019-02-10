86c02cc6086fc8a081ef66fa28f9ba7f84a92a64
gpanaggio <31071203+gpanaggio@users.noreply.github.com>
2019-02-10 01:12:30 -0300

    Nombres modificados de point

--- Tp_final/Tp_final/point.h
+++ Tp_final/Tp_final/point.h
@@ -1,13 +1,12 @@
-#pragma once
 #ifndef point_h
 #define point_h
 
-class POINT
+class POINT_
 {
 public:
-	POINT(float x, float y);
-	POINT();
-	~POINT();
+	POINT_(float x, float y);
+	POINT_();
+	~POINT_();
 	float get_x_coord();
 	float get_y_coord();
 	void set_x_coord(float x);
