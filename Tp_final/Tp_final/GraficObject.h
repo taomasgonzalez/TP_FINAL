de7a57eff6851ce2e49a2e3df1a62db7405b41c9
gpanaggio <31071203+gpanaggio@users.noreply.github.com>
2019-02-10 01:14:56 -0300

    Add files via upload

--- Tp_final/Tp_final/GraficObject.h
+++ Tp_final/Tp_final/GraficObject.h
@@ -18,7 +18,6 @@
 
 #define BLOCK_SIZE (50)		// pixeles en X e Y que tiene un bloque de pantalla
 
-#define GRAVITY (6.0)			// supongo una gravedad de 6 m/s
 #define JUMP_TIME (1.2)			// tiempo de salto -> 1.2s			(lo que tarda en llegar 2 bloques mas arriba)
 
 #define MOVE_RATIO_FALL	(0.3)											// velocidad de caida (300ms/bloque)
@@ -34,27 +33,26 @@ public:
 	Obj_Graf();
 	~Obj_Graf();
 	Obj_Graf(double ID);
-	virtual void startDraw(Direction dir, void *estado, POINT& pos) = 0;			// a startDraw lo llama el modelo del juego y le marca donde iniciar la secuencia de dibujo y en que dirección y estado
+	virtual void startDraw(Direction dir, void *estado, POINT_& pos) = 0;			// a startDraw lo llama el modelo del juego y le marca donde iniciar la secuencia de dibujo y en que dirección y estado
 	virtual void destroy() = 0;
 	virtual void draw() = 0;
 	void setID(double ID);											// setea el ID
-	void setPos(POINT);
+	void setPos(POINT_);
 	void setPos(double x, double y);								// coordenadas del objeto en el mapa
-	void setInitalPos(POINT pos);
+	void setInitalPos(POINT_ pos);
 	bool isActive();
+	virtual void reset() = 0;
+	virtual bool secuenceOver() = 0;
 
 protected:
 	double ID;							// identificador del objeto grafico
 //	ALLEGRO_BITMAP *** images = NULL;			// triple puntero porque por el bitmap ya tenes 1 y vas a tener varios arreglos para cada estado
 	unsigned int actualImage;			// te marca que posicion de arreglo estas para poder pasar a la siguiente
 	Direction dir;
-	POINT pos;
+	POINT_ pos;
 	bool active;						// el objeto garfico va a estar activo cuando se halla iniciado su secuencia de dibujo
 	int velX;							// velocidad de desplazamiento horizontal (andando) (en pixeles/sec)
-	int initialVelY;					// valocidad inicial al momento del salto
 	int velFall;						// velocidad de caida
-	POINT InitalPos;					// posición inicial para saber cuando detener la animación, y pasivar el objeto
-//	int fallTicks;						// medida de tiempo de caida
-	int jumpTicks;						// medida de tiempo de salto (reciclar variables -> mala practica)
-
+	POINT_ InitalPos;					// posición inicial para saber cuando detener la animación, y pasivar el objeto
+	bool secuenceOver_;
 };
