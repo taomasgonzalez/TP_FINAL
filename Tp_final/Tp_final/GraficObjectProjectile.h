be51264c879feb10d9bbe1d246f8e40a504ecb16
gpanaggio <31071203+gpanaggio@users.noreply.github.com>
2019-02-10 01:17:41 -0300

    Add files via upload

--- Tp_final/Tp_final/GraficObjectProjectile.h
+++ Tp_final/Tp_final/GraficObjectProjectile.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "GraficObject.h"
 
-enum PROYECTILE_TYPE {SNOW, FIRE};
+enum PROYECTILE_TYPE {SNOW, FIRE};		// ¿la bola es un proyectil?
 
-enum PROYECTILE_STATE {proy_MOVING, proy_DECAYING, proy_IMPACT};		// el proyectil decae cuando se detiene a los 3 casilleros y se cae
+enum PROYECTILE_STATE {proy_MOVING, proy_DECAYING, proy_IMPACT, proy_FALLING};		// el proyectil decae cuando se detiene a los 3 casilleros y se cae
 
 
 class Obj_Graf_Projectile :
@@ -15,8 +15,10 @@ public:
 	~Obj_Graf_Projectile();
 
 	void draw();
-	void startDraw(Direction dir, void *state, POINT& pos);
+	void startDraw(Direction dir, void *state, POINT_& pos);
 	void destroy();
+	void reset();
+	bool secuenceOver();
 
 private:
 	PROYECTILE_TYPE type;
@@ -25,6 +27,8 @@ private:
 	ALLEGRO_BITMAP ** moveImages = NULL;
 	ALLEGRO_BITMAP ** decayImages = NULL;
 	ALLEGRO_BITMAP ** impactImages = NULL;
+	ALLEGRO_BITMAP ** fallImages = NULL;
 	unsigned int actualImpactImage;										// cuando se inicia la secuancia de impacto tiene que empezar si o si de 0 (el actualImage puede no estar en 0)
+	unsigned int actualDecayImage;
 };
 
