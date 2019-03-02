54a63847f6b4353f098d31b1094d1b0d94db3729
taomasgonzalez <togonzalez@itba.edu.ar>
2019-03-02 17:12:26 -0300

    nuevas cosas de escena para germo

--- Tp_final/Tp_final/CharacterSceneObserver.h
+++ Tp_final/Tp_final/CharacterSceneObserver.h
@@ -11,6 +11,8 @@ public:
 	~CharacterSceneObserver();
 
 	void update();
+protected:
+	virtual void perform_character_movement(EventPackage* action, bool& should_die, bool& should_hit);
 private:
 	Scene * scenario = NULL;
 	Character* character = NULL;
@@ -18,6 +20,7 @@ private:
 	CharacterActionsEventGenerator* ev_gen = NULL;
 
 	void kill_character();
+	void append_action_to_character(Action_info action);
 };
 
 
