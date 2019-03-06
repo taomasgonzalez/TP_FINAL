68295708d57367d60d7ea6a40dfeda6835abc508
taomasgonzalez <togonzalez@itba.edu.ar>
2019-03-05 22:44:50 -0300

    Arregle EnemyFSM

--- Tp_final/Tp_final/EnemyActionsFSM.cpp
+++ Tp_final/Tp_final/EnemyActionsFSM.cpp
@@ -12,27 +12,48 @@ void enemy_die_r(void* data);
 
 EnemyActionsFSM::EnemyActionsFSM(Enemy* enemy): CharacterActionsFSM(enemy)
 {
-	expand_state(iddle_state, {Event_type::GOT_HIT, freezing_state, got_hit_r});
-	
-	freezing_state->push_back({Event_type::GOT_HIT, freezing_state, got_hit_r});
+	this->enemy = enemy;
+	set_states();
+
+	this->actual_state = iddle_state;
+
+	set_processes();
+	create_all_timers();
+}
+
+
+EnemyActionsFSM::~EnemyActionsFSM()
+{
+	delete freezing_state;
+	delete frozen_state;
+}
+void EnemyActionsFSM::set_states()
+{
+
+	expand_state(iddle_state, { Event_type::GOT_HIT, freezing_state, got_hit_r });
+
+	freezing_state = new std::vector<edge_t>();
+	frozen_state = new std::vector<edge_t>();
+
+	freezing_state->push_back({ Event_type::GOT_HIT, freezing_state, got_hit_r });
 	freezing_state->push_back({ Event_type::FROZE, frozen_state, freeze_r });
 	freezing_state->push_back({ Event_type::PARTIALLY_UNFROZE, freezing_state, partially_unfroze_r });
 	freezing_state->push_back({ Event_type::UNFROZE, iddle_state, unfroze_r });
 	freezing_state->push_back({ Event_type::END_OF_TABLE, iddle_state, do_nothing_enemy_r });
 
 	frozen_state->push_back({ Event_type::GOT_HIT, frozen_state, start_moving_snowball_r });
-	frozen_state->push_back({Event_type::MOVE, frozen_state, snowball_move_r});
-	frozen_state->push_back({ Event_type::DIED, dead_state, enemy_die_r});
+	frozen_state->push_back({ Event_type::MOVE, frozen_state, snowball_move_r });
+	frozen_state->push_back({ Event_type::DIED, dead_state, enemy_die_r });
 	frozen_state->push_back({ Event_type::END_OF_TABLE, frozen_state, do_nothing_enemy_r });
-
-	this->actual_state = iddle_state;
 }
 
 
-EnemyActionsFSM::~EnemyActionsFSM()
-{
-}
+void EnemyActionsFSM::set_processes() {
 
+}
+void EnemyActionsFSM::create_all_timers() {
+
+}
 void do_nothing_enemy_r(void* data) {
 
 }
