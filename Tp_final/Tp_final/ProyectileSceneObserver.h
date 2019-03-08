dd8fc12398a96c0d65ffdca29ab4a96af146cf21
taomasgonzalez <togonzalez@itba.edu.ar>
2019-03-08 18:25:15 -0300

    Empece con observers para ProyectileActionsFSM

--- Tp_final/Tp_final/ProyectileSceneObserver.h
+++ Tp_final/Tp_final/ProyectileSceneObserver.h
@@ -1,14 +1,21 @@
 #pragma once
 #include "Scene.h"
+#include "ProyectilesActionsFSM.h"
 
-class ProyectileSceneObserver 
+class ProyectileSceneObserver : public Observer
 {
 public:
 	ProyectileSceneObserver(Proyectile* proy, Scene * scenario);
 	~ProyectileSceneObserver();
+	void update();
+
 protected:
 	Proyectile * proy = NULL;
+	void perform_logical_movement();
 private:
 	Scene * scenario = NULL;
+	EventGenerator* ev_gen = NULL;
+	ProyectilesActionsFSM * fsm = NULL;
+
 };
 
--- Tp_final/Tp_final/ProyectilesActionsFSMDRAWObserver.cpp
+++ Tp_final/Tp_final/ProyectilesActionsFSMDRAWObserver.cpp
@@ -21,14 +21,15 @@ void ProyectilesActionsFSMDRAWObserver::update() {
 		drawer->startDraw(proy_MOVING, proyectile->id, direction_translation(fsm->get_current_action_direction()), proyectile->pos_x, proyectile->pos_y);
 		curr_state = proy_MOVING;
 	}
-	else if (fsm->obs_info.start_impacting_graph) {
+	if (fsm->obs_info.start_impacting_graph) {
 		drawer->startDraw(proy_IMPACT, proyectile->id, direction_translation(fsm->get_current_action_direction()), proyectile->pos_x, proyectile->pos_y);
 		curr_state = proy_IMPACT;
 	}
-	else if (fsm->obs_info.start_falling_graph) {
+	if (fsm->obs_info.start_falling_graph) {
 		drawer->startDraw(proy_FALLING, proyectile->id, direction_translation(fsm->get_current_action_direction()), proyectile->pos_x, proyectile->pos_y);
 		curr_state = proy_FALLING;
 	}
+
 }
 
 Direction direction_translation(Direction_type dir) {
