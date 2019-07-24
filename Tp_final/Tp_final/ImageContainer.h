5d3e488c16cc7482f491e6fca581d3cccbb9c804
taomasgonzalez <togonzalez@itba.edu.ar>
2019-07-24 13:08:03 -0300

    Arreglo imagenes para que se maneje todo estaticamente

---
+++ Tp_final/Tp_final/ImageContainer.h
@@ -0,0 +1,95 @@
+#pragma once
+#include <allegro5/allegro5.h> 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_color.h> 
+#include <allegro5/allegro_primitives.h> 
+#include <allegro5/allegro_image.h> 
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_audio.h>
+#include "point.h"
+#include <string>
+#include <math.h>				/* pow */
+#include <iostream>
+#include "EnemyDefines.h"
+#include "PlayerDefines.h"
+#include "ProjectileDefine.h"
+
+class ImageContainer
+{
+public:
+	ImageContainer();
+	~ImageContainer();
+
+	struct enemy_images {
+		ALLEGRO_BITMAP ** attackImages = NULL;
+		ALLEGRO_BITMAP ** walkImages = NULL;
+		ALLEGRO_BITMAP ** jumpImages = NULL;
+		ALLEGRO_BITMAP ** idleImages = NULL;
+		ALLEGRO_BITMAP ** fallImages = NULL;
+		ALLEGRO_BITMAP ** trap1Images = NULL;
+		ALLEGRO_BITMAP ** trap2Images = NULL;
+		ALLEGRO_BITMAP ** dieImages = NULL;
+		// estas son la imagenes de la bola
+		ALLEGRO_BITMAP ** inballIdleImages = NULL;
+		ALLEGRO_BITMAP ** inballMoveImages = NULL;
+		ALLEGRO_BITMAP ** inballPushImages = NULL;
+		ALLEGRO_BITMAP ** inballFallImages = NULL;
+		ALLEGRO_BITMAP ** inballDestructiontImages = NULL;
+	};
+
+	struct enemies_images_container{
+		enemy_images purple;
+		enemy_images crazy;
+		enemy_images fatty;
+	};
+
+	enemies_images_container my_enemy_images;
+
+	struct character_images {
+		ALLEGRO_BITMAP ** walkImages = NULL;
+		ALLEGRO_BITMAP ** jumpImages = NULL;
+		ALLEGRO_BITMAP ** idleImages = NULL;
+		ALLEGRO_BITMAP ** attackImages = NULL;
+		ALLEGRO_BITMAP ** fallImages = NULL;
+		ALLEGRO_BITMAP ** pushImages = NULL;
+		ALLEGRO_BITMAP ** dieImages = NULL;
+	};
+
+	struct character_images_container {
+		character_images tom;
+		character_images nick;
+	};
+	character_images_container my_character_images_container;
+
+	struct projectile_images {
+		ALLEGRO_BITMAP ** moveImages = NULL;
+		ALLEGRO_BITMAP ** decayImages = NULL;
+		ALLEGRO_BITMAP ** impactImages = NULL;
+		ALLEGRO_BITMAP ** fallImages = NULL;
+	};
+
+	struct projectile_images_container {
+		projectile_images snowball;
+		projectile_images fire;
+	};
+
+	projectile_images_container my_projectile_images_container;
+
+private:
+	//enemy bitmaps
+	void load_enemy_bitmaps();
+	void load_purple_bitmaps();
+	void load_fatty_bitmaps();
+	void load_crazy_bitmaps();
+
+	//projectile bitmaps
+	void load_projectile_bitmaps();
+	void load_snowball_bitmaps();
+	void load_fire_bitmaps();
+
+	//character bitmaps
+	void load_character_bitmaps();
+	void load_tom_bitmaps();
+	void load_nick_bitmaps();
+};
+
