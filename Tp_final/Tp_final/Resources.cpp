24b8161d6f944081936a92477b8f050a738551b4
taomasgonzalez <togonzalez@itba.edu.ar>
2019-08-18 11:57:32 -0300

    corrijo error bldo

--- Tp_final/Tp_final/Resources.cpp
+++ Tp_final/Tp_final/Resources.cpp
@@ -73,7 +73,7 @@ void Resources::add_all_observers() {
 	my_logic_fsm->add_observer(new FSMCommunicationObserver(my_logic_fsm,my_communication, my_scenario, my_user_data));
 	my_logic_fsm->add_observer(new LogicFSMEventsObserver(my_logic_ev_gen, my_logic_fsm, my_allegro_container, my_user_data, my_scenario));
 	my_logic_fsm->add_observer(new LogicFSMSceneObserver(my_logic_fsm, my_scenario, my_logic_ev_gen, my_user_data));
-	my_logic_fsm->add_observer(new LogicFSMGraphicEventsObserver(my_logic_fsm, my_graphic_ev_gen);
+	my_logic_fsm->add_observer(new LogicFSMGraphicEventsObserver(my_logic_fsm, my_graphic_ev_gen));
 
 	my_scenario->add_observer(new ScenarioEventsObserver(my_logic_ev_gen, my_scenario, my_logic_fsm, my_user_data));
 	my_scenario->add_observer(new ScenarioDRAWObserver(my_scenario, my_drawer));
