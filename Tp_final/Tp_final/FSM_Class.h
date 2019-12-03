785cd4506574f6d7e1db4c45284a74232fb9d9de
taomasgonzalez <togonzalez@itba.edu.ar>
2019-12-02 21:13:12 -0300

    right now map display working!
    will start changing the fsm class for better debugging

--- Tp_final/Tp_final/FSM_Class.h
+++ Tp_final/Tp_final/FSM_Class.h
@@ -11,7 +11,7 @@
 #include <string>
 
 using namespace std;
-
+//changing fsm class!
 /*****************************************************
 *          DEFINITION OF EDGE
 * **************************************************/
