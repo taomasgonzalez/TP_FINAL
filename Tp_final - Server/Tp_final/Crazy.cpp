24a4ba727c2d76ff389b75277070759ea5fd4357
Germán Bertachini <gbertachini@itba.edu.ar>
2020-02-08 00:36:18 -0300

    Revert "Merge branch 'enemy_movements' into Smoothing_grapichs"
    This reverts commit 70a813911d2a511a62f40993429fdcf7357ec3d8, reversing
    changes made to e2427976908d4d9bb603532a1910bd56e5dd556e.

--- Tp_final - Server/Tp_final/Crazy.cpp
+++ Tp_final - Server/Tp_final/Crazy.cpp
@@ -31,27 +31,29 @@ Crazy::~Crazy()
 Action_info Crazy::act() {
 
 	Action_info returnable_EA;
-	returnable_EA.valid = false;
-
+	//al_stop_timer(acting_timer);
 	double sample = acting_probabilities(generator);
 
-	if (sample <= 0.75) {
-		if (!move_in_same_direction(&returnable_EA))
-			sample = 0.8;
-	}	
-	else {
-		sample = acting_probabilities(generator);
+	while (!returnable_EA.valid)
+		if (sample <= 0.75) {
+			if (!move_in_same_direction(&returnable_EA))
+				sample = 0.8;
+		}	
+		else {
+			sample = acting_probabilities(generator);
 
-		if ((sample >= 0.0) && (sample <= 1.0 / 3.0)) {
-			if (!move_in_opposite_direction(&returnable_EA)) 
-				sample = 0.5;
+			while (!returnable_EA.valid) {
+				if ((sample >= 0.0) && (sample <= 1.0 / 3.0)) {
+					if (!move_in_opposite_direction(&returnable_EA)) 
+						sample = 0.5;
+				}
+				else if ((sample >= 1.0 / 3.0) && (sample <= 2.0 / 3.0)) 
+					stay_still(&returnable_EA);
+				else 
+					if(!jump(&returnable_EA))
+						sample = 0.1;
+			}
 		}
-		else if ((sample >= 1.0 / 3.0) && (sample <= 2.0 / 3.0)) 
-			stay_still(&returnable_EA);
-		else 
-			if(!jump(&returnable_EA))
-				sample = 0.1;
-	}
 
 	if (EA_package_ID == FINISH_POINT_PACKAGE_ID_FOR_EA)
 		EA_package_ID = START_POINT_PACKAGE_ID_FOR_EA;
@@ -62,8 +64,7 @@ Action_info Crazy::act() {
 
 	//al_set_timer_speed(acting_timer, timer_speed);
 	//al_start_timer(acting_timer);
-	if(!returnable_EA.valid)
-		stay_still(&returnable_EA);
+
 
 	return returnable_EA;
 }
